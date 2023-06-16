ef6366d665e5b34cc2936b9b827da5c79a56b9b6
devsrpereira <srpereira@uni9.edu.br>
2023-06-16 13:54:00 -0300

    @Bean para inializar o banco já com alguns valores

--- Crypton--App/src/main/java/com/srdevpereira/controller/CoinController.java
+++ Crypton--App/src/main/java/com/srdevpereira/controller/CoinController.java
@@ -3,10 +3,12 @@ package com.srdevpereira.controller;
 import com.srdevpereira.entities.Coin;
 import com.srdevpereira.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @RestController
@@ -15,6 +17,34 @@ public class CoinController {
 
     @Autowired
     private CoinRepository coinRepository;
+
+    @Bean
+    public Coin init(){
+        Coin c1 = new Coin();
+        c1.setName("BITCOIN");
+        c1.setPrice(new BigDecimal(100));
+        c1.setQuantity(new BigDecimal(0.005));
+        c1.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c2 = new Coin();
+        c2.setName("BITCOIN");
+        c2.setPrice(new BigDecimal(200));
+        c2.setQuantity(new BigDecimal(0.01));
+        c2.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c3 = new Coin();
+        c3.setName("ETHEREUM");
+        c3.setPrice(new BigDecimal(500));
+        c3.setQuantity(new BigDecimal(0.100));
+        c3.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        coinRepository.insert(c1);
+        coinRepository.insert(c2);
+        coinRepository.insert(c3);
+
+        return c1;
+    }
+
 
     @GetMapping()
     public ResponseEntity get(){
